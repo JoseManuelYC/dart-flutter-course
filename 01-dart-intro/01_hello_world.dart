c4fe2b8d7c7045c172e353e2063a99308d6f1791
JoseManuelYC <joseyanezcontact@gmail.com>
2025-09-21 19:20:26 -0400

    feat: initial commit

---
+++ 01-dart-intro/01_hello_world.dart
@@ -0,0 +1,12 @@
+void main() {
+  //var myName = "Jose";
+  //final myName = "Jose";
+  // late final myName = "Jose";
+  //const myName = "Jose";
+
+  String myName = "Jose";
+
+  myName.toUpperCase();
+
+  print(`Hello ${myname} dev`); 
+}
