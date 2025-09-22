2122a91d4e7c5b0e89c80827de90de9104d73a71
JoseManuelYC <joseyanezcontact@gmail.com>
2025-09-21 22:03:06 -0400

    feat: dynamic type

--- 01-dart-intro/02_variables.dart
+++ 01-dart-intro/02_variables.dart
@@ -5,6 +5,12 @@ void main(){
   final List<String> abilities = ["Lanza Fuego"];
   final sprites = <String>['charmander/front.png', "charmander/back.png"];
   
+  dynamic errorMessage = "Error";
+  errorMessage = [1,2,3,4];
+  errorMessage = {1,2,3,4};
+  errorMessage = () => true;
+  errorMessage = null;
+  
   
   
   print("""
@@ -13,6 +19,8 @@ void main(){
   Is-Alive: $isAlive
   Abilities: $abilities
   sprites: $sprites
+  
+  Error: $errorMessage
   
   """);
 }
