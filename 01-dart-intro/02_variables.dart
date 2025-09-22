3890f693083c1f615144863d4f8d8480b1fad5d1
JoseManuelYC <joseyanezcontact@gmail.com>
2025-09-21 22:04:24 -0400

    feat: add comments

--- 01-dart-intro/02_variables.dart
+++ 01-dart-intro/02_variables.dart
@@ -5,6 +5,8 @@ void main(){
   final List<String> abilities = ["Lanza Fuego"];
   final sprites = <String>['charmander/front.png', "charmander/back.png"];
   
+
+  //dynamic == null
   dynamic errorMessage = "Error";
   errorMessage = [1,2,3,4];
   errorMessage = {1,2,3,4};
