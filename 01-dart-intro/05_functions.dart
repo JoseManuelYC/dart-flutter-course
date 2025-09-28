eed35b75f2ba9e73bf199fac08a67c7b28bef43e
JoseManuelYC <joseyanezcontact@gmail.com>
2025-09-28 19:16:22 -0400

    feat: add parameters required and optionals

--- 01-dart-intro/05_functions.dart
+++ 01-dart-intro/05_functions.dart
@@ -1,7 +1,13 @@
 void main(){
+  
+  
   print(greetEveryone());
+  
   print("Suma: ${addNumbers(10, 20)}");
   print("Suma: ${addNumbersOptional(10)}");
+  
+  
+  print(greetPerson(name: "Jose M", message: "HIII,"));
 }
 
 String greetEveryone(){
@@ -17,4 +23,8 @@ int addNumbersOptional(int a , [ int b = 0 ]){
 int addNumbersOptional2(int a , [ int? b ]){
   b ??= 0;
   return a + b ;
+}
+
+String greetPerson({ required String name, String message = "Hola," }){
+  return '${message} $name';
 }
